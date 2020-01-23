ee5ff1f351e97f782ae07438c535803c392432cb
ths12 <tomasz.halas7@gmail.com>
2020-01-24 00:12:09 +0100

    Updates

---
+++ precache-manifest.d29a68cf727678d2181d68cb758a5436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1a9906387ce5652811989c6a465daf",
+    "url": "/robofriends-th/index.html"
+  },
+  {
+    "revision": "ac72a426b8be0457fcce",
+    "url": "/robofriends-th/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7efffbc5ed94101d8426",
+    "url": "/robofriends-th/static/css/main.a2701282.chunk.css"
+  },
+  {
+    "revision": "ac72a426b8be0457fcce",
+    "url": "/robofriends-th/static/js/2.1865bbc9.chunk.js"
+  },
+  {
+    "revision": "7efffbc5ed94101d8426",
+    "url": "/robofriends-th/static/js/main.081b668b.chunk.js"
+  },
+  {
+    "revision": "a2352089dc388eb15d23",
+    "url": "/robofriends-th/static/js/runtime-main.25b1bc63.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-th/static/media/SEGA.12a3db8b.woff"
+  }
+]);
